332399aae080a37cfbf78f3243c04810c2c9fb4e
Nursyifa Azizah <nursyifaziza@gmail.com>
2023-08-19 17:08:27 +0800

    Add new route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './Home';
+import VideoDetail from './VideoDetail';
 
 function App() {
     return (
         <Router>
             <Routes>
                 <Route exact path="/" element={< Home />}/>
+                <Route path="/video/:videoId" element={< VideoDetail />}/>
             </Routes>
         </Router>
     );
