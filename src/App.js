b66d7cb08f0fd3cb787f971b77a26f7e8495b12c
Nursyifa Azizah <nursyifaziza@gmail.com>
2023-08-19 23:35:46 +0800

    Install Chakra UI

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
+import {ChakraProvider} from '@chakra-ui/react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './Home';
 import VideoDetail from './VideoDetail';
 
 function App() {
     return (
-        <Router>
-            <Routes>
-                <Route exact path="/" element={< Home />}/>
-                <Route path="/video/:videoId" element={< VideoDetail />}/>
-            </Routes>
-        </Router>
+        <ChakraProvider>
+            <Router>
+                <Routes>
+                    <Route exact path="/" element={< Home />}/>
+                    <Route path="/video/:videoId" element={< VideoDetail />}/>
+                </Routes>
+            </Router>
+        </ChakraProvider>
     );
 }
 
